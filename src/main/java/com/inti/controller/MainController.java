b00993acc3782bea6e7f677f6b9cd022b2c5b261
theod <theod@DESKTOP-VBIVKTD>
2023-03-15 12:30:35 +0100

    delete + update

--- src/main/java/com/inti/controller/MainController.java
+++ src/main/java/com/inti/controller/MainController.java
@@ -2,8 +2,10 @@ package com.inti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.inti.model.Voiture;
@@ -29,4 +31,38 @@ public class MainController {
 		
 		return "redirect:/formVoiture";
 	}
+	
+	@GetMapping("listeVoiture")
+	public String listeVoiture(Model m)
+	{
+		m.addAttribute("listeV", ivr.findAll());
+		
+		return "listeVoiture";
+	}
+	
+	@GetMapping("deleteVoiture/{id}")
+	public String deleteVoiture(@PathVariable("id") int id)
+	{
+		ivr.deleteById(id);
+		
+		return "redirect:/listeVoiture";
+	}
+	
+	@GetMapping("modifVoiture/{id}")
+	public String modifVoiture(@PathVariable("id") int id, Model m)
+	{
+		m.addAttribute("voiture", ivr.getReferenceById(id));
+		
+		return "modifVoiture";
+	}
+	
+	@PostMapping("updateVoiture")
+	public String updateVoiture(@ModelAttribute("voit") Voiture v)
+	{ 
+		// comme saveOrUpdate
+		ivr.save(v);
+		
+		return "redirect:/listeVoiture";
+	}
+	
 }
